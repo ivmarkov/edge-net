88561785a6307125759a334af18ee630784fffa3
ivmarkov <ivan.markov@gmail.com>
2023-01-17 18:54:52 +0000

    Fixes for connection splitting

--- src/asynch/http/client.rs
+++ src/asynch/http/client.rs
@@ -41,7 +41,7 @@ where
         })
     }
 
-    pub fn reinitialize(&mut self, addr: SocketAddr) -> Result<(), Error<T::Error>> {
+    pub async fn reinitialize(&mut self, addr: SocketAddr) -> Result<(), Error<T::Error>> {
         let _ = self.complete().await;
         self.unbound_mut().unwrap().addr = addr;
 
