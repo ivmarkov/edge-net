8d14d938ac5672abb83b4acd50d2152ccfd72637
ivmarkov <ivan.markov@gmail.com>
2023-12-25 22:18:29 +0000

    Less confusing IO client and server

--- edge-dhcp/src/io/client.rs
+++ edge-dhcp/src/io/client.rs
@@ -3,7 +3,7 @@ use core::fmt::Debug;
 use embassy_futures::select::{select, Either};
 use embassy_time::{Duration, Instant, Timer};
 
-use embedded_nal_async::{ConnectedUdp, Ipv4Addr};
+use embedded_nal_async::Ipv4Addr;
 
 use log::{info, warn};
 
@@ -14,268 +14,222 @@ pub use super::*;
 pub use crate::Settings;
 use crate::{Options, Packet};
 
-#[derive(Clone, Debug)]
-pub struct Configuration {
-    pub socket: SocketAddrV4,
-    pub server_port: u16,
-    pub mac: [u8; 6],
-    pub timeout_secs: u32,
+/// Represents the additional network-related information that might be returned by the DHCP server.
+#[derive(Debug, Clone)]
+pub struct NetworkInfo {
+    pub gateway: Option<Ipv4Addr>,
+    pub subnet: Option<Ipv4Addr>,
+    pub dns1: Option<Ipv4Addr>,
+    pub dns2: Option<Ipv4Addr>,
 }
 
-impl Configuration {
-    pub const fn new(mac: [u8; 6]) -> Self {
-        Self {
-            socket: SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, DEFAULT_CLIENT_PORT),
-            server_port: DEFAULT_SERVER_PORT,
-            mac,
-            timeout_secs: 10,
-        }
-    }
+/// Represents a DHCP IP lease.
+/// 
+/// This structure has a set of asynchronous methods that can utilize a supplied DHCP client instance and UDP socket to 
+/// transparently implement all aspects of negotiating an IP with the DHCP server and then keeping the lease of that IP up to date.
+#[derive(Debug, Clone)]
+pub struct Lease {
+    pub ip: Ipv4Addr,
+    pub server_ip: Ipv4Addr,
+    pub duration: Duration,
+    pub acquired: Instant,
 }
 
-/// A simple asynchronous DHCP client.
-///
-/// The client takes a UDP socket stack and then takes care of the all the negotiations with the DHCP server,
-/// as in discovering servers, negotiating initial IP, and then keeping the lease of that IP up to date.
-///
-/// Note that the `UdpStack` implementation that the client takes need to be a bit special, because the DHCP client operates
-/// before it has an IP address assigned:
-/// - It needs to be able to send broadcast packets (to IP 255.255.255.255)
-/// - It needs to be able to receive broadcast packets
-///
-/// Usually, this is achieved by utilizing raw sockets. One such socket stack is `Udp2RawStack` in the `edge-raw` crate.
-pub struct Client<'a, T, F> {
-    stack: F,
-    buf: &'a mut [u8],
-    client: dhcp::client::Client<T>,
-    socket: SocketAddrV4,
-    server_port: u16,
-    timeout: Duration,
-    pub settings: Option<(Settings, Instant)>,
-}
-
-impl<'a, T, F> Client<'a, T, F>
-where
-    T: RngCore,
-    F: UdpStack,
-{
-    pub fn new(stack: F, buf: &'a mut [u8], rng: T, conf: &Configuration) -> Self {
-        info!("Creating DHCP client with configuration {conf:?}");
-
-        Self {
-            stack,
-            buf,
-            client: dhcp::client::Client { rng, mac: conf.mac },
-            socket: conf.socket,
-            server_port: conf.server_port,
-            timeout: Duration::from_secs(conf.timeout_secs as _),
-            settings: None,
-        }
-    }
-
-    /// Runs the DHCP client and takes care of all aspects of negotiating an IP with the first DHCP server that replies to the discovery requests.
-    ///
-    /// From the POV of the user, this method will return only in two cases, which are exactly the cases where the user is expected to take an action:
-    /// - When an initial/new IP lease was negotiated; in that case, `Some(Settings)` is returned, and the user should assign the returned IP settings
-    ///   to the network interface using platform-specific means
-    /// - When the IP lease was lost; in that case, `None` is returned, and the user should de-assign all IP settings from the network interface using
-    ///   platform-specific means
-    ///
-    /// In both cases, user is expected to call `run` again, so that the IP lease is kept up to date / a new lease is re-negotiated
-    ///
-    /// Note that dropping this future is also safe in that it won't remove the current lease, so the user can renew
-    /// the operation of the client by just calling `run` later on. Of course, if the future is not polled, the client
-    /// would be unable - during that time - to check for lease timeout and the lease might not be renewed on time.
-    ///
-    /// But in any case, if the lease is expired or the DHCP server does not acknowledge the lease renewal, the client will
-    /// automatically restart the DHCP servers' discovery from the very beginning.
-    pub async fn run(&mut self) -> Result<Option<Settings>, Error<F::Error>> {
+impl Lease {
+    /// Creates a new DHCP lease by discovering a DHCP server and requesting an IP from it.
+    /// This is done by utilizing the supplied DHCP client instance and UDP socket.
+    /// 
+    /// Note that the supplied UDP socket should be capable of sending and receiving broadcast UDP packets.
+    pub async fn new<T, S>(client: &mut dhcp::client::Client<T>, socket: &mut S, buf: &mut [u8]) -> Result<(Self, NetworkInfo), Error<S::Error>>
+    where 
+        T: RngCore,
+        S: UnconnectedUdp,
+    {
         loop {
-            if let Some((settings, acquired)) = self.settings.as_ref() {
-                // Keep the lease
-                let now = Instant::now();
+            let offer = Self::discover(client, socket, buf, Duration::from_secs(3)).await?;
 
-                if now - *acquired
-                    >= Duration::from_secs(settings.lease_time_secs.unwrap_or(7200) as u64 / 3)
-                {
-                    info!("Renewing DHCP lease...");
+            let now = Instant::now();
 
-                    if let Some(settings) = self
-                        .request(settings.server_ip.unwrap(), settings.ip)
-                        .await?
-                    {
-                        self.settings = Some((settings, Instant::now()));
-                    } else {
-                        // Lease was not renewed; let the user know
-                        self.settings = None;
-
-                        return Ok(None);
-                    }
-                } else {
-                    Timer::after(Duration::from_secs(60)).await;
-                }
-            } else {
-                // Look for offers
-                let offer = self.discover().await?;
-
-                if let Some(settings) = self.request(offer.server_ip.unwrap(), offer.ip).await? {
-                    // IP acquired; let the user know
-                    self.settings = Some((settings.clone(), Instant::now()));
-
-                    return Ok(Some(settings));
-                }
+            if let Some(settings) = Self::request(client, socket, buf, offer.server_ip.unwrap(), offer.ip, true, Duration::from_secs(3), 3).await? {
+                break Ok((Self {
+                    ip: settings.ip,
+                    server_ip: settings.server_ip.unwrap(),
+                    duration: Duration::from_secs(settings.lease_time_secs.unwrap_or(7200) as _),
+                    acquired: now,
+                }, NetworkInfo {
+                    gateway: settings.gateway,
+                    subnet: settings.subnet,
+                    dns1: settings.dns1,
+                    dns2: settings.dns2,
+                }));
             }
         }
     }
 
-    /// This method allows the user to inform the DHCP server that the currently leased IP (if any) is no longer used
-    /// by the client.
-    ///
-    /// Useful when the program runnuing the DHCP client is about to exit.
-    pub async fn release(&mut self) -> Result<(), Error<F::Error>> {
-        if let Some((settings, _)) = self.settings.as_ref().cloned() {
-            let server_ip = settings.server_ip.unwrap();
-            let (_, mut socket) = self
-                .stack
-                .connect_from(
-                    SocketAddr::V4(self.socket),
-                    SocketAddr::V4(SocketAddrV4::new(server_ip, self.server_port)),
-                )
-                .await
-                .map_err(Error::Io)?;
+    /// Keeps the DHCP lease up to date by renewing it when necessary using the supplied DHCP client instance and UDP socket.
+    pub async fn keep<T, S>(&mut self, client: &mut dhcp::client::Client<T>, socket: &mut S, buf: &mut [u8]) -> Result<(), Error<S::Error>>
+    where 
+        T: RngCore,
+        S: UnconnectedUdp,
+    {
+        loop {
+            let now = Instant::now();
 
-            let mut opt_buf = Options::buf();
-            let request = self.client.release(&mut opt_buf, 0, settings.ip);
-
-            socket
-                .send(request.encode(self.buf)?)
-                .await
-                .map_err(Error::Io)?;
+            if now - self.acquired >= self.duration / 3 {
+                if !self.renew(client, socket, buf).await? {
+                    // Lease was not renewed; let the user know
+                    break;
+                }
+            } else {
+                Timer::after(Duration::from_secs(60)).await;
+            }
         }
-
-        self.settings = None;
 
         Ok(())
     }
 
-    async fn discover(&mut self) -> Result<Settings, Error<F::Error>> {
+    /// Renews the DHCP lease by utilizing the supplied DHCP client instance and UDP socket.
+    pub async fn renew<T, S>(&mut self, client: &mut dhcp::client::Client<T>, socket: &mut S, buf: &mut [u8]) -> Result<bool, Error<S::Error>>
+    where 
+        T: RngCore,
+        S: UnconnectedUdp,
+    {
+        info!("Renewing DHCP lease...");
+
+        let now = Instant::now();
+        let settings = Self::request(client, socket, buf, self.server_ip, self.ip, false, Duration::from_secs(3), 3).await?;
+
+        if let Some(settings) = settings.as_ref() {
+            self.duration = settings.lease_time_secs.map(|lt| Duration::from_secs(lt as _)).unwrap_or(self.duration);
+            self.acquired = now;
+        }
+
+        Ok(settings.is_some())
+    }
+
+    /// Releases the DHCP lease by utilizing the supplied DHCP client instance and UDP socket.
+    pub async fn release<T, S>(self, client: &mut dhcp::client::Client<T>, socket: &mut S, buf: &mut [u8]) -> Result<(), Error<S::Error>>
+    where 
+        T: RngCore,
+        S: UnconnectedUdp,
+    {
+        let mut opt_buf = Options::buf();
+        let request = client.release(&mut opt_buf, 0, self.ip);
+
+        socket
+            .send(
+                SocketAddr::V4(SocketAddrV4::new(self.ip, DEFAULT_CLIENT_PORT)),
+                SocketAddr::V4(SocketAddrV4::new(self.server_ip, DEFAULT_SERVER_PORT)),
+                request.encode(buf)?)
+            .await
+            .map_err(Error::Io)?;
+
+        Ok(())
+    }
+
+    async fn discover<T, S>(
+        client: &mut dhcp::client::Client<T>,
+        socket: &mut S,
+        buf: &mut [u8],
+        timeout: Duration,
+    ) -> Result<Settings, Error<S::Error>> 
+    where 
+        T: RngCore,
+        S: UnconnectedUdp,
+    {
         info!("Discovering DHCP servers...");
 
         let start = Instant::now();
 
         loop {
-            let mut socket = self
-                .stack
-                .bind_multiple(SocketAddr::V4(SocketAddrV4::new(
-                    Ipv4Addr::UNSPECIFIED,
-                    self.socket.port(),
-                )))
-                .await
-                .map_err(Error::Io)?;
-
             let mut opt_buf = Options::buf();
 
             let (request, xid) =
-                self.client
+                client
                     .discover(&mut opt_buf, (Instant::now() - start).as_secs() as _, None);
 
             socket
                 .send(
-                    SocketAddr::V4(self.socket),
-                    SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::BROADCAST, self.server_port)),
-                    request.encode(self.buf)?,
+                    SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, DEFAULT_CLIENT_PORT)),
+                    SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::BROADCAST, DEFAULT_SERVER_PORT)),
+                    request.encode(buf)?,
                 )
                 .await
                 .map_err(Error::Io)?;
 
-            let offer_start = Instant::now();
+            if let Either::First(result) = select(socket.receive_into(buf), Timer::after(timeout)).await {
+                let (len, _local, _remote) = result.map_err(Error::Io)?;
+                let reply = Packet::decode(&buf[..len])?;
 
-            while Instant::now() - offer_start < self.timeout {
-                let timer = Timer::after(Duration::from_secs(3));
+                if client.is_offer(&reply, xid) {
+                    let settings: Settings = (&reply).into();
 
-                if let Either::First(result) = select(socket.receive_into(self.buf), timer).await {
-                    let (len, _local, _remote) = result.map_err(Error::Io)?;
-                    let reply = Packet::decode(&self.buf[..len])?;
+                    info!(
+                        "IP {} offered by DHCP server {}",
+                        settings.ip,
+                        settings.server_ip.unwrap()
+                    );
 
-                    if self.client.is_offer(&reply, xid) {
-                        let settings: Settings = (&reply).into();
-
-                        info!(
-                            "IP {} offered by DHCP server {}",
-                            settings.ip,
-                            settings.server_ip.unwrap()
-                        );
-
-                        return Ok(settings);
-                    }
+                    return Ok(settings);
                 }
             }
 
-            drop(socket);
-
-            info!("No DHCP offers received, sleeping for a while...");
-
-            Timer::after(Duration::from_secs(3)).await;
+            info!("No DHCP offers received, retrying...");
         }
     }
 
-    async fn request(
-        &mut self,
+    async fn request<T, S>(
+        client: &mut dhcp::client::Client<T>,
+        socket: &mut S,
+        buf: &mut [u8],
         server_ip: Ipv4Addr,
         ip: Ipv4Addr,
-    ) -> Result<Option<Settings>, Error<F::Error>> {
-        for _ in 0..3 {
+        broadcast: bool,
+        timeout: Duration,
+        retries: usize,
+    ) -> Result<Option<Settings>, Error<S::Error>> 
+    where 
+        T: RngCore,
+        S: UnconnectedUdp,
+    {
+        for _ in 0..retries {
             info!("Requesting IP {ip} from DHCP server {server_ip}");
-
-            let (_, mut socket) = self
-                .stack
-                .bind_single(SocketAddr::V4(self.socket))
-                .await
-                .map_err(Error::Io)?;
 
             let start = Instant::now();
 
             let mut opt_buf = Options::buf();
 
             let (request, xid) =
-                self.client
-                    .request(&mut opt_buf, (Instant::now() - start).as_secs() as _, ip);
+                client
+                    .request(&mut opt_buf, (Instant::now() - start).as_secs() as _, ip, broadcast);
 
             socket
                 .send(
-                    SocketAddr::V4(self.socket),
-                    SocketAddr::V4(SocketAddrV4::new(server_ip, self.server_port)),
-                    request.encode(self.buf)?,
+                    SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, DEFAULT_CLIENT_PORT)),
+                    SocketAddr::V4(SocketAddrV4::new(if broadcast { Ipv4Addr::BROADCAST } else { server_ip }, DEFAULT_SERVER_PORT)),
+                    request.encode(buf)?,
                 )
                 .await
                 .map_err(Error::Io)?;
 
-            let request_start = Instant::now();
+            if let Either::First(result) = select(socket.receive_into(buf), Timer::after(timeout)).await {
+                let (len, _local, _remote) = result.map_err(Error::Io)?;
+                let packet = &buf[..len];
 
-            while Instant::now() - request_start < self.timeout {
-                let timer = Timer::after(Duration::from_secs(10));
+                let reply = Packet::decode(packet)?;
 
-                if let Either::First(result) = select(socket.receive_into(self.buf), timer).await {
-                    let (len, _local, _remote) = result.map_err(Error::Io)?;
-                    let packet = &self.buf[..len];
+                if client.is_ack(&reply, xid) {
+                    let settings = (&reply).into();
 
-                    let reply = Packet::decode(packet)?;
+                    info!("IP {} leased successfully", ip);
 
-                    if self.client.is_ack(&reply, xid) {
-                        let settings = (&reply).into();
+                    return Ok(Some(settings));
+                } else if client.is_nak(&reply, xid) {
+                    info!("IP {} not acknowledged", ip);
 
-                        info!("IP {} leased successfully", ip);
-
-                        return Ok(Some(settings));
-                    } else if self.client.is_nak(&reply, xid) {
-                        info!("IP {} not acknowledged", ip);
-
-                        return Ok(None);
-                    }
+                    return Ok(None);
                 }
             }
-
-            drop(socket);
         }
 
         warn!("IP request was not replied");
--- edge-dhcp/src/io/server.rs
+++ edge-dhcp/src/io/server.rs
@@ -1,8 +1,6 @@
-use core::fmt::Debug;
-
-use embassy_time::Duration;
-
 use embedded_nal_async::Ipv4Addr;
+
+use embedded_nal_async_xtra::UnconnectedUdpWithMac;
 
 use log::{info, warn};
 
@@ -10,129 +8,75 @@ use self::dhcp::{Options, Packet};
 
 pub use super::*;
 
-#[derive(Clone, Debug)]
-pub struct Configuration<'a> {
-    pub socket: SocketAddrV4,
-    pub ip: Ipv4Addr,
-    pub gateways: &'a [Ipv4Addr],
-    pub subnet: Option<Ipv4Addr>,
-    pub dns: &'a [Ipv4Addr],
-    pub range_start: Ipv4Addr,
-    pub range_end: Ipv4Addr,
-    pub lease_duration_secs: u32,
-}
-
-impl<'a> Configuration<'a> {
-    pub fn new(ip: Ipv4Addr, gw_buf: Option<&'a mut [Ipv4Addr; 1]>) -> Self {
-        let octets = ip.octets();
-
-        let gateways = if let Some(gw_buf) = gw_buf {
-            gw_buf[0] = ip;
-            gw_buf.as_slice()
-        } else {
-            &[]
-        };
-
-        Self {
-            socket: SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, DEFAULT_SERVER_PORT),
-            ip,
-            gateways,
-            subnet: Some(Ipv4Addr::new(255, 255, 255, 0)),
-            dns: &[],
-            range_start: Ipv4Addr::new(octets[0], octets[1], octets[2], 50),
-            range_end: Ipv4Addr::new(octets[0], octets[1], octets[2], 200),
-            lease_duration_secs: 7200,
-        }
-    }
-}
-
-/// A simple asynchronous DHCP server.
+/// Runs the provided DHCP server asynchronously using the supplied UDP socket and server options.
 ///
-/// The server takes a UDP socket stack and then processes all incoming BOOTP requests, by updating its internal simple database of leases,
-/// and issuing replies.
+/// All incoming BOOTP requests are processed by updating the DHCP server's internal simple database of leases,
+/// and by issuing replies.
 ///
-/// Note that the `UdpStack` implementation that the server takes need to be a bit special, because DHCP clients operate
-/// before they have an IP address assigned:
-/// - It needs to be able to send broadcast packets (to IP 255.255.255.255)
-/// - It needs to be able to receive broadcast packets
-pub struct Server<'a, F, const N: usize = 64> {
-    stack: F,
-    buf: &'a mut [u8],
-    socket: SocketAddrV4,
-    server_options: dhcp::server::ServerOptions<'a>,
-    pub server: dhcp::server::Server<N>,
-}
-
-impl<'a, F, const N: usize> Server<'a, F, N>
+/// Dropping this future is safe in that it won't remove the internal leases' database,
+/// so users are free to drop the future in case they would like to take a snapshot of the leases or inspect them otherwise.
+/// 
+/// Note that the UDP socket that the server takes need to be capable of sending and receiving broadcast UDP packets.
+/// 
+/// Furthermore, some DHCP clients do send DHCP OFFER packets without the broadcast flag in the DHCP payload being set to true.
+/// To support these clients, the socket needs to also be capable of sending packets with a broadcast IP destination address 
+/// - yet - with the destination *MAC* address in the Ethernet frame set to the MAC address of the DHCP client.
+/// 
+/// Such UDP sockets implement the `UnconnectedUdpWithMac` trait and are essentially based on the raw socket functionality, 
+/// as available on most operating systems.
+pub async fn run<T, const N: usize>(
+    server: &mut dhcp::server::Server<N>,
+    server_options: &dhcp::server::ServerOptions<'_>,
+    socket: &mut T,
+    buf: &mut [u8],
+) -> Result<(), Error<T::Error>>
 where
-    F: UdpStack,
+    T: UnconnectedUdp + UnconnectedUdpWithMac,
 {
-    pub fn new(stack: F, buf: &'a mut [u8], conf: &Configuration<'a>) -> Self {
-        info!("Creating DHCP server with configuration {conf:?}");
+    info!("Running DHCP server for addresses {}-{} with configuration {server_options:?}", server.range_start, server.range_end);
 
-        Self {
-            stack,
-            buf,
-            socket: conf.socket,
-            server_options: dhcp::server::ServerOptions {
-                ip: conf.ip,
-                gateways: conf.gateways,
-                subnet: conf.subnet,
-                dns: conf.dns,
-                lease_duration: Duration::from_secs(conf.lease_duration_secs as _),
-            },
-            server: dhcp::server::Server {
-                range_start: conf.range_start,
-                range_end: conf.range_end,
-                leases: heapless::LinearMap::new(),
-            },
-        }
-    }
-
-    /// Runs the DHCP server, processing incoming DHCP requests.
-    ///
-    /// Note that dropping this future is safe in that it won't remove the internal leases' database,
-    /// so users are free to drop the future in case they would like to take a snapshot of the leases or inspect them otherwise.
-    pub async fn run(&mut self) -> Result<(), Error<F::Error>> {
-        let (_, mut socket) = self
-            .stack
-            .bind_single(SocketAddr::V4(self.socket))
+    loop {
+        let (len, local, remote) = UnconnectedUdp::receive_into(socket, buf)
             .await
             .map_err(Error::Io)?;
+        let packet = &buf[..len];
 
-        loop {
-            let (len, local, remote) = socket.receive_into(self.buf).await.map_err(Error::Io)?;
-            let packet = &self.buf[..len];
+        let request = match Packet::decode(packet) {
+            Ok(request) => request,
+            Err(err) => {
+                warn!("Decoding packet returned error: {:?}", err);
+                continue;
+            }
+        };
 
-            let request = match Packet::decode(packet) {
-                Ok(request) => request,
-                Err(err) => {
-                    warn!("Decoding packet returned error: {:?}", err);
-                    continue;
-                }
-            };
+        let mut opt_buf = Options::buf();
 
-            let mut opt_buf = Options::buf();
-
-            if let Some(reply) =
-                self.server
-                    .handle_request(&mut opt_buf, &self.server_options, &request)
-            {
-                let remote = if let SocketAddr::V4(socket) = remote {
-                    if request.broadcast || *socket.ip() == Ipv4Addr::UNSPECIFIED {
-                        SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::BROADCAST, socket.port()))
-                    } else {
-                        remote
-                    }
+        if let Some(reply) = server.handle_request(&mut opt_buf, server_options, &request) {
+            let remote = if let SocketAddr::V4(socket) = remote {
+                if request.broadcast || *socket.ip() == Ipv4Addr::UNSPECIFIED {
+                    SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::BROADCAST, socket.port()))
                 } else {
                     remote
-                };
+                }
+            } else {
+                remote
+            };
 
-                socket
-                    .send(local, remote, reply.encode(self.buf)?)
-                    .await
-                    .map_err(Error::Io)?;
-            }
+            let remote_mac = if request.broadcast {
+                [0xff; 6]
+            } else {
+                request.chaddr[..6].try_into().unwrap()
+            };
+
+            UnconnectedUdpWithMac::send(
+                socket,
+                local,
+                remote,
+                Some(&remote_mac),
+                reply.encode(buf)?,
+            )
+            .await
+            .map_err(Error::Io)?;
         }
     }
 }
